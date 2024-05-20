66b84e35ae8fc177e7672c1d06ece61c401e866b
@Helen853 <dudarevalen_1997@mail.ru>
2024-05-20 23:57:21 +0500

    Создан начальный проект

---
+++ CombineTextField/CombineTextField/CombineTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineTextFieldApp.swift
+//  CombineTextField
+//
+//  Created by Киса Мурлыса on 20.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct CombineTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineTextField/CombineTextField/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CombineTextField
+//
+//  Created by Киса Мурлыса on 20.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
