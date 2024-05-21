3ade01de045ffd7ba324309da94a7657c7583a42
@Helen853 <dudarevalen_1997@mail.ru>
2024-05-21 17:57:36 +0500

    Создан начальный проект

---
+++ Basket/Basket/BasketApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasketApp.swift
+//  Basket
+//
+//  Created by Киса Мурлыса on 21.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct BasketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
