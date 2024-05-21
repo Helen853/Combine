5706472a87ac3af69652234bbd17e360b6314d0c
@Helen853 <dudarevalen_1997@mail.ru>
2024-05-21 23:46:33 +0500

    Создан начальный проект

---
+++ Numbers/Numbers/NumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumbersApp.swift
+//  Numbers
+//
+//  Created by Киса Мурлыса on 21.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct NumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
