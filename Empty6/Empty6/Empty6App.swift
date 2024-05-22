a9e6797dae3117d92615f77c5404569ae69c6438
@Helen853 <dudarevalen_1997@mail.ru>
2024-05-23 00:08:38 +0500

    Сделала empty

---
+++ Empty6/Empty6/Empty6App.swift
@@ -0,0 +1,17 @@
+//
+//  Empty6App.swift
+//  Empty6
+//
+//  Created by Киса Мурлыса on 22.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Empty6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
