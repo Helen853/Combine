159f92b5d96d3a456fea807fe6545b1ae871255d
@Helen853 <dudarevalen_1997@mail.ru>
2024-05-26 17:20:16 +0500

    Создан начальный проект

---
+++ RikAndMortySUI/RikAndMortySUI/RikAndMortySUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RikAndMortySUIApp.swift
+//  RikAndMortySUI
+//
+//  Created by Киса Мурлыса on 26.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct RikAndMortySUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
